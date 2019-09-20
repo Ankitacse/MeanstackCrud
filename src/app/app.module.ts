72e2cb11276a02492a598a508d3661659a5e2bcd
ankitacse <ankitacse005@gmail.com>
2019-09-20 15:47:56 +0530

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { CoustomMaterailModule } from './coustom-materail/coustom-materail.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent
   ],
   imports: [
+    ReactiveFormsModule,
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    CoustomMaterailModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/coustom-materail/coustom-materail.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import {
+  MatButtonModule, MatIconModule, MatMenuModule, MatToolbarModule,
+  MatDividerModule, MatFormFieldModule, MatInputModule, MatDialogModule,
+  MatProgressSpinnerModule, MatSelectModule, MatDatepickerModule,
+  MatNativeDateModule, MatCardModule, MatSlideToggleModule, MatListModule,
+  MatStepperModule, MatRadioModule, MatCheckboxModule, MatExpansionModule,
+  MatProgressBarModule, MatSidenavModule, MatChipsModule, MatTooltipModule,
+  MatTabsModule, MatTableModule,MatTreeModule, MatAutocompleteModule
+} from '@angular/material';
+import {} from '@angular/material/tree';
+
+@NgModule({
+ 
+  exports:[
+    MatButtonModule, MatIconModule, MatMenuModule, MatToolbarModule, MatDividerModule,
+    MatFormFieldModule, MatInputModule, MatDialogModule, MatProgressSpinnerModule,
+    MatSelectModule, MatDatepickerModule, MatNativeDateModule, MatCardModule,
+    MatSlideToggleModule, MatListModule, MatStepperModule, MatRadioModule,
+    MatCheckboxModule, MatExpansionModule, MatProgressBarModule,
+    MatSidenavModule, MatChipsModule, MatTooltipModule,
+    MatTabsModule, MatTableModule, MatAutocompleteModule,MatTreeModule
+  ]
+})
+export class CoustomMaterailModule { }
