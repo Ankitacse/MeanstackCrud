4df8c6d75c9077746ea9f3dbbbc332100b3b89db
ankitacse <ankitacse005@gmail.com>
2019-10-01 13:19:58 +0530

    delete Modifyed

--- backend/app.js
+++ backend/app.js
@@ -39,11 +39,13 @@ app.post("/api/posts", (req, res, next) => {
         title: req.body.title,
         content: req.body.content
     });
-    post.save();
-    console.log(post);
-    res.status(201).json({
-        message: 'Post added successfully'
+    post.save().then(createdPost => {
+        res.status(201).json({
+            message: 'Post added successfully',
+            postId:createdPost._id
+        });
     });
+   
 });
 
 app.get('/api/posts', (req, res, next) => {
@@ -57,7 +59,8 @@ app.get('/api/posts', (req, res, next) => {
 });
 
 app.delete("/api/posts/:id", (req, res, next) => {
-    Post.deleteOne({ _id: req.params.id }).then(result => {
+    Post.deleteOne({ _id: req.params.id })
+    .then(result => {
         console.log(result);
         res.status(200).json({
             message: 'Post Deleted'
