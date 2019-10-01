81c4f18f64c97dd887a63a1f55e82c19fc43fe0c
ankitacse <ankitacse005@gmail.com>
2019-10-01 12:47:08 +0530

    update frontend after delete content

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -49,9 +49,12 @@ export class PostsService {
 
     deletePost(postId: string) {
         console.log('This Post is Deleted');
-        this.http.delete('http://localhost:3000/api/posts/'  + postId).subscribe(() =>{
-            console.log('Deleted !');
-        });
+        this.http.delete('http://localhost:3000/api/posts/' + postId)
+            .subscribe(() => {
+                const updatedPosts = this.posts.filter(post => post.id !== postId);
+                this.posts = updatedPosts;
+                this.postsUpdated.next([...this.posts])
+            });
     }
 
 } 
