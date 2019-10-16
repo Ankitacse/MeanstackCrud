96edd88c9690fc3afaec98752eafe6074d57ae06
ankitacse <ankitacse005@gmail.com>
2019-10-16 19:35:17 +0530

    login_signup_form Ui done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
-import {  ReactiveFormsModule } from '@angular/forms';
+import {  ReactiveFormsModule,FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { CoustomMaterailModule } from './coustom-materail/coustom-materail.module';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { PostsService } from './services/post.service';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 @NgModule({
   declarations: [
@@ -17,8 +19,11 @@ import { HttpClientModule } from '@angular/common/http';
     PostCreateComponent,
     PostListComponent,
     HeaderComponent,
+    LoginComponent,
+    SignupComponent,
   ],
   imports: [
+    FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
     BrowserModule,
