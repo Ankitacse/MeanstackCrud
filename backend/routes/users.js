43c94e6619cd3a6579e1069cf048dbd5244e4153
ankitacse <ankitacse005@gmail.com>
2019-10-18 17:59:28 +0530

    bcryptJsAdded

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,32 +1,28 @@
+// sudo npm i --save bcryptjs
 const express = require("express");
 const User = require("../models/user")
 const router = express.Router();
-// const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 router.post("/signup", (req, res, next) => {
-    
-   const user = new User({
-       email:req.body.email,
-       password:req.body.password
-   })
+    bcrypt.hash(req.body.password, 10).then(hash => {
+        const user = new user({
+            email: req.body.email,
+            // password:req.body.password
+            password: hash
+        });
+        user.save().then(result => {
+            res.status(201).json({
+                message: 'User created !',
+                result: result
+            })
+        }).catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        })
+    });
 
 });
 
 module.exports = router;
 
-// bcrypt.hash(req.body.password, 10).then(hash => {
-    //     const user = new user({
-    //         email: req.body.email,
-    //         // password:req.body.password
-    //         password: hash
-    //     });
-    //     user.save().then(result => {
-    //         res.status(201).json({
-    //             message: 'User created !',
-    //             result: result
-    //         })
-    //     }).catch(err=>{
-    //         res.status(500).json({
-    //             error:err
-    //         })
-    //     })
-    // });
